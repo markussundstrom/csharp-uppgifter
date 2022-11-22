f4e397e4bc207446e0acb6806afd0f880a80fd3b
Markus Sundström <sundstrom.markus@gmail.com>
2022-11-22 09:25:08 +0100

    2-27

---
+++ 2-27-1/2-27-1/Program.cs
@@ -0,0 +1,37 @@
+﻿namespace _2_27_1
+{
+
+    internal class Program
+    {
+        static void Main(string[] args) {
+            bool running = true;
+            Stack stack = new Stack();
+            Console.WriteLine("a: add value r: remove value c: clear q: quit");
+            while (running) {
+                switch (Console.ReadKey(true).KeyChar) {
+                    case 'a':
+                        Console.WriteLine("Add value:");
+                        String input = Console.ReadLine();
+                        stack.Push(input);
+                        break;
+                    case 'r':
+                        try {
+                            Console.WriteLine(stack.Pop());
+                        }
+                        catch (Exception e) {
+                            Console.WriteLine(e.Message);
+                        }
+                        break;
+                    case 'c':
+                        stack.Clear();
+                        break;
+                    case 'q':
+                        running = false;
+                        break;
+                    default:
+                        break;
+                }
+            }
+        }
+    }
+}
---
+++ 2-27-1/2-27-1/Stack.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+
+namespace _2_27_1
+{
+    public class Stack {
+        private List<Object> _objectlist = new List<Object>();
+
+        public void Push(Object obj) {
+            if (obj == null) {
+                throw new InvalidOperationException("Can't push null to stack");
+            } else {
+                _objectlist.Add(obj);
+            }
+        }
+
+        public Object Pop() {
+            if (this._objectlist.Count == 0) {
+                throw new InvalidOperationException("Can't pop empty list");
+            } else {
+                Object last = _objectlist[_objectlist.Count - 1];
+                _objectlist.RemoveAt(_objectlist.Count - 1);
+                return last;
+            }
+        }
+
+        public void Clear() {
+            _objectlist.Clear();
+        }
+    }
+}
